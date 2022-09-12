9172c2739589fbf458aa3e7e8fb27cf3a3112c24
Thiago Côrtes <thiago_silvacortes@hotmail.com>
2022-09-12 19:24:38 -0300

    first commit

---
+++ api-server-config/src/main/java/com/cortes/ms/api/server/config/ApiServerConfigApplication.java
@@ -0,0 +1,13 @@
+package com.cortes.ms.api.server.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiServerConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiServerConfigApplication.class, args);
+	}
+
+}
