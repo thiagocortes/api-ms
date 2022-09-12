d4808a8f02c7404be7650544b216263aa6daf892
Thiago Côrtes <thiago_silvacortes@hotmail.com>
2022-09-12 19:40:43 -0300

    another

---
+++ api-registry/src/main/java/com/cortes/ms/api/registry/ApiRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.cortes.ms.api.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRegistryApplication.class, args);
+	}
+
+}
---
+++ api-registry/src/test/java/com/cortes/ms/api/registry/ApiRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cortes.ms.api.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
